2906529cb09aa135e2e74daa547423f92491f322
Suraj7852 <surajpj7852@gmail.com>
2019-12-13 10:34:30 +0530

    Added test to us Census if wrong file path sent

--- src/test/java/censusanalyser/USCensusAdapterTest.java
+++ src/test/java/censusanalyser/USCensusAdapterTest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class USCensusAdapterTest {
 
     private static final String US_CENSUS_CSV_FILE_PATH = "./src/test/resources/USCensusData.csv";
-
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     @Test
     public void givenUSCensusCSV_ShouldReturnExactCount() {
         USCensusAdapter usCensusAdapter = new USCensusAdapter();
@@ -17,6 +17,16 @@ public class USCensusAdapterTest {
             Assert.assertEquals(51,usCensusData.size());
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenUSCensusCSV_ThrowsExceptionWhen_WhenWrongFilePathSent() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            Map<String, CensusDAO> usCensusData = usCensusAdapter.loadCensusData(CensusAnalyser.Country.US,WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 }
