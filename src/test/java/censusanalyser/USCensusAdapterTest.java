25275dc1ef62b7a73d4e4037aa9ef9e724cd8582
Suraj7852 <surajpj7852@gmail.com>
2019-12-13 10:38:26 +0530

    Added test to us Census if header mismatches

--- src/test/java/censusanalyser/USCensusAdapterTest.java
+++ src/test/java/censusanalyser/USCensusAdapterTest.java
@@ -9,6 +9,7 @@ public class USCensusAdapterTest {
 
     private static final String US_CENSUS_CSV_FILE_PATH = "./src/test/resources/USCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_HEADER = "./src/test/resources/USCensusWrongHeader.csv";
     @Test
     public void givenUSCensusCSV_ShouldReturnExactCount() {
         USCensusAdapter usCensusAdapter = new USCensusAdapter();
@@ -27,6 +28,16 @@ public class USCensusAdapterTest {
             Map<String, CensusDAO> usCensusData = usCensusAdapter.loadCensusData(CensusAnalyser.Country.US,WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenUSCensusCSV_ThrowsExceptionWhen_HeaderMisMatches() {
+        USCensusAdapter usCensusAdapter = new USCensusAdapter();
+        try {
+            usCensusAdapter.loadCensusData(CensusAnalyser.Country.US,WRONG_CSV_FILE_HEADER);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.HEADER_MISMATCH,e.type);
         }
     }
 }
