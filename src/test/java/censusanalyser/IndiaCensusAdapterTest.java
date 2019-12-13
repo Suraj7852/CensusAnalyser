ab042ecca9e21b78e201d09732bcccda9068bd2f
Suraj7852 <surajpj7852@gmail.com>
2019-12-13 10:27:45 +0530

    Added test if state code file path sent first throws exception

--- src/test/java/censusanalyser/IndiaCensusAdapterTest.java
+++ src/test/java/censusanalyser/IndiaCensusAdapterTest.java
@@ -16,7 +16,9 @@ public class IndiaCensusAdapterTest {
             IndiaCensusAdapter censusAnalyser = new IndiaCensusAdapter();
             Map<String, CensusDAO> stringCensusCount = censusAnalyser.loadCensusData(CensusAnalyser.Country.INDIA, INDIA_CENSUS_CSV_FILE_PATH, INDIA_STATE_CSV_FILE_PATH);
             Assert.assertEquals(29,stringCensusCount.size());
-        } catch (CensusAnalyserException e) { }
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -26,6 +28,16 @@ public class IndiaCensusAdapterTest {
             censusAnalyser.loadCensusData(CensusAnalyser.Country.INDIA, WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenStateCodeSentFirst_ThrowsException() {
+        try {
+            IndiaCensusAdapter censusAnalyser = new IndiaCensusAdapter();
+            censusAnalyser.loadCensusData(CensusAnalyser.Country.INDIA,  INDIA_STATE_CSV_FILE_PATH,INDIA_CENSUS_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.HEADER_MISMATCH,e.type);
         }
     }
 }
